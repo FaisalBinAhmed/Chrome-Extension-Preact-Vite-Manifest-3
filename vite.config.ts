db01ad49e425fed0756fb04a68921614fd5c9c02
Faisal Bin Ahmed <faisalbin@outlook.com>
2023-04-19 17:31:35 +0200

    vite and tsc

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,13 @@ import preact from "@preact/preset-vite";
 
 export default defineConfig({
 	plugins: [preact()],
+	build: {
+		rollupOptions: {
+			input: {
+				main: "./src/action.html",
+				options: "./src/options/options.html",
+				// background: "./src/background"
+			},
+		},
+	},
 });
